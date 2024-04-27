5b2efbf0a27e7ed97cd4f9308a542c199b6c2ec6
Joben Gabin <Jobengabin@lpulaguna.edu.ph>
2024-04-27 13:26:37 +0800

    another one

--- project4.py
+++ project4.py
@@ -9,36 +9,36 @@ class SpeechRecognizerApp:
         self.master = master
         self.master.title("Speech Recognition")
 
-        # Choose Language
+        
         self.language_label = tk.Label(master, text="Select Target Language:")
         self.language_label.pack()
 
-        self.languages = ["English", "French", "German", "Spanish"]  # Add more languages as needed
+        self.languages = ["English", "French", "German", "Spanish", "Filipino", "Japanese"]  # Add more languages as needed
         self.selected_language = tk.StringVar()
         self.language_dropdown = ttk.Combobox(master, textvariable=self.selected_language, values=self.languages)
         self.language_dropdown.pack()
 
-        # Choose Microphone
+        
         self.microphone_label = tk.Label(master, text="Select Microphone:")
         self.microphone_label.pack()
 
         self.available_microphones = sr.Microphone().list_microphone_names()
         self.selected_microphone = tk.StringVar()
-        self.microphone_dropdown = ttk.Combobox(master, textvariable=self.selected_microphone, values=self.available_microphones)
+        self.microphone_dropdown = ttk.Combobox(master, textvariable=self.selected_microphone, values=self.available_microphones, width=50) # Adjust width as needed
         self.microphone_dropdown.pack()
 
-        # Start and stop recording buttons
+      
         self.start_button = tk.Button(master, text="Start Recording", command=self.start_recording)
         self.start_button.pack()
 
         self.stop_button = tk.Button(master, text="Stop Recording", command=self.stop_recording, state=tk.DISABLED)
         self.stop_button.pack()
 
-        # Text area for original speech
+     
         self.text_area = scrolledtext.ScrolledText(master, width=40, height=10)
         self.text_area.pack()
 
-        # Text area for translated text
+     
         self.translated_text_area = scrolledtext.ScrolledText(master, width=40, height=10)
         self.translated_text_area.pack()
 
@@ -67,7 +67,7 @@ class SpeechRecognizerApp:
                     recognized_text = r.recognize_google(audio)
                     self.text_area.insert(tk.END, recognized_text + '\n')
 
-                    # Translate recognized text to the selected language
+                    
                     translated_text = translator.translate(recognized_text, dest=self.selected_language.get())
                     self.translated_text_area.insert(tk.END, translated_text.text + '\n')
 
@@ -87,6 +87,7 @@ class SpeechRecognizerApp:
 
 def main():
     root = tk.Tk()
+    root.geometry("500x720")
     app = SpeechRecognizerApp(root)
     root.mainloop()
 
